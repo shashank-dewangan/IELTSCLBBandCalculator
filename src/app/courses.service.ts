806ec46835c5597a2f16d535b741e73597e3d5fa
shashank-dewangan <shashank.at30@gmail.com>
2017-12-21 15:41:21 +0530

    Directives changes

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -6,6 +6,6 @@ export class CoursesService {
   constructor() { }
 
   getCourses(){
-    return ['course 1','course 2','course 3'];
+    return ['course 1','course 2','course 3','course 4','course 5'];
   }
 }
